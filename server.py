6ef586fa67c6d6739aa8baef843cfa67802dc003
sajitheranda <sajitheranda227@gmail.com>
2023-10-26 22:51:14 +0530

    1

--- server.py
+++ server.py
@@ -1,10 +1,6 @@
 from flask import Flask
-
-app = Flask(__name)
+app = Flask(__name__)
 
 @app.route('/')
-def home():
+def hello_world():
     return 'Hello, World!'
-
-if __name__ == '__main__':
-    app.run()
